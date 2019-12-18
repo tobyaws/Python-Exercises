7b5b70b1dda935cf96f8428c0de945a2d0831a28
tobyaws <57716137+tobyaws@users.noreply.github.com>
2019-12-18 20:34:16 +0000

    Delete Hello Admin.py

--- Hello Admin.py
+++
@@ -1,6 +0,0 @@
-username = ["admin", "toby", "eric", "phil", "tom"]
-for user in username:
-    if user is "admin":
-        print("Hello admin, would you like to see a status report?")
-    else:
-        print("Hello %s, thank you for logging in again." % user)
