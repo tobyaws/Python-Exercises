b99b6857270c168f10aa4b829b6b75279d43da9a
tobyaws <57716137+tobyaws@users.noreply.github.com>
2019-12-31 16:06:30 +0000

    Add files via upload

---
+++ Ordinal Numbers.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in numbers:
+    if i == 1:
+        print(str(i) + "st")
+    elif i == 2:
+        print(str(i) + "nd")
+    elif i == 3:
+        print(str(i) + "rd")
+    else:
+        print(str(i) + "th")
