c76fd9a6906748b2d6bf79912ba154ed411d81c5
tobyaws <57716137+tobyaws@users.noreply.github.com>
2020-01-02 14:25:01 +0000

    Add files via upload

---
+++ Large Shirts.py
@@ -0,0 +1,9 @@
+def make_shirt(size="L", text="I love Python"):
+    print('Shirt size: {0} text on shirt: "{1}"'.format(size, text))
+
+
+# default
+make_shirt()
+
+# custom
+make_shirt(size="M", text="I hate JavaScript")
