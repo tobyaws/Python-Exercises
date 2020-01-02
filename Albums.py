beafe453e539bc3cc1d7a8294e57abb6d02dd066
tobyaws <57716137+tobyaws@users.noreply.github.com>
2020-01-02 14:59:53 +0000

    Add files via upload

---
+++ Albums.py
@@ -0,0 +1,9 @@
+def make_album(artist, album_title):
+    return {artist: album_title}
+
+
+print(make_album("Tame Impala ", "Currents"))
+
+print(make_album("Jamie xx", "In Colour"))
+
+print(make_album("Bonobo", "Migration"))
